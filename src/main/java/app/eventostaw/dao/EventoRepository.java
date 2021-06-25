4c334257a697d715d824c342b233d029ff5e20d4
EloxZ <45355879+EloxZ@users.noreply.github.com>
2021-06-25 17:46:50 +0200

    limpiar

--- src/main/java/app/eventostaw/dao/EventoRepository.java
+++
@@ -1,7 +0,0 @@
-package app.eventostaw.dao;
-
-import app.eventostaw.entity.Evento;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EventoRepository extends JpaRepository<Evento, Integer> {
-}
