36ef919aa1d556ee53eb99b5069920f9a5fc5e13
EloxZ <45355879+EloxZ@users.noreply.github.com>
2021-06-25 17:48:08 +0200

    aa

--- src/main/java/app/eventostaw/dao/UsuarioRepository.java
+++
@@ -1,8 +0,0 @@
-package app.eventostaw.dao;
-
-import app.eventostaw.entity.Usuario;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
-
-}
