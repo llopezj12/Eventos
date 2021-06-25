f6db5a8b02fdbea70b8dd5d60575766493c418c1
EloxZ <45355879+EloxZ@users.noreply.github.com>
2021-06-25 13:02:51 +0200

    Limpieza

--- src/main/java/es/eventos/eventos/dao/EventoRepository.java
+++
@@ -1,7 +0,0 @@
-package es.eventos.eventos.dao;
-
-import es.eventos.eventos.entity.Evento;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EventoRepository extends JpaRepository<Evento, Integer> {
-}
--- src/main/java/es/eventos/eventos/dao/UsuarioRepository.java
+++
@@ -1,7 +0,0 @@
-package es.eventos.eventos.dao;
-
-import es.eventos.eventos.entity.Usuario;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
-}
