f1f0971522aeddd2ffea1e5feb12900bf4e307ce
Luis López <0619696966@uma.es>
2021-06-26 21:05:38 +0200

    CRUD Creador de eventos

--- src/main/java/app/eventostaw/dao/EventoRepository.java
+++ src/main/java/app/eventostaw/dao/EventoRepository.java
@@ -1,9 +1,16 @@
 package app.eventostaw.dao;
 
 import app.eventostaw.entity.Evento;
+import app.eventostaw.entity.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 import java.util.List;
 
 public interface EventoRepository extends JpaRepository<Evento, Integer> {
+    @Query("SELECT e FROM Evento e WHERE e.idCreador = :idCreador")
+    public List<Evento> findByIdCreador(@Param("idCreador") Integer idCreador);
 }
